5c246c46a1db56b75f9bf597b129852f962e1f5d
Anndraj <amirrezasolhjo2004@gmail.com>
2024-03-12 18:01:44 +0330

    Added Method : Added string LastName for the capitalizing fist letter in lastname

--- Answers/Project.java
+++ Answers/Project.java
@@ -7,6 +7,7 @@ public class Project {
         Scanner Last = new Scanner(System.in);
         String last = Last.nextLine();
         String FirstName = first.substring(0,1).toUpperCase() + first.substring(1).toLowerCase();
+        String LastName = last.substring(0,1).toUpperCase() + last.substring(1).toLowerCase();
 
     }
 }
