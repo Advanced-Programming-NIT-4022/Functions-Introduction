439b7878c532f641a26fd63652ad6782d9e09b65
Anndraj <amirrezasolhjo2004@gmail.com>
2024-03-13 00:17:02 +0330

    Added the method for getting studentid and checking that is it between 4 to 13 digits or no. no going for a loop until it's between 4 to 13 digits

--- Answers/Project.java
+++ Answers/Project.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+class Userid{
+    public static String Idnum() {
+        Scanner scr = new Scanner(System.in);
+        String id = "";
+        while (true) {
+            System.out.println("Enter Your IdNumber: ");
+            id = scr.nextLine();
+            if (id.length() >= 4 && id.length() <= 13 && id.matches("[0-9]+"))
+                break;
+            else {
+                System.out.println("Invalid Id. please enter number between 4 & 13 !");
+            }
+        }
+        return id;
+    }
+}
+
 
 public class Project {
         public static boolean isValidPhoneNumber(String phoneNumber) {
@@ -8,13 +25,16 @@ public class Project {
         public static String addZeroToPhoneNumber(String phoneNumber) {
             return "0" + phoneNumber;
         }
+
         public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         System.out.println("1.Setting Name");
         System.out.println("2.Setting Phone");
         System.out.println("3.Setting Id");
         System.out.println("4.Setting Interests");
         int in = input.nextInt();
+
         if(in == 1 ){
             Scanner First = new Scanner(System.in);
             System.out.println("Enter The FirstName : ");
@@ -22,29 +42,32 @@ public class Project {
             Scanner Last = new Scanner(System.in);
             System.out.println("Enter The LastName : ");
             String last = Last.nextLine();
+
             String FirstName = first.substring(0,1).toUpperCase()
                     + first.substring(1).toLowerCase();
             String LastName = last.substring(0,1).toUpperCase()
                     + last.substring(1).toLowerCase();
             String FullName = FirstName + " " + LastName;
+
             System.out.println("saving...");
             System.out.println();
             System.out.println("FullName Is : " + FullName);
+
             System.out.println();
             System.out.println("2.Setting Phone");
             System.out.println("3.Setting Id");
             System.out.println("4.Setting Interests");
-            in = input.nextInt();
-            if( in == 2){
-                        Scanner inputphone = new Scanner(System.in);
 
-                        String phoneNumber = "";
-                        boolean isValid = false;
+            Scanner input1 = new Scanner(System.in);
+            int in1 = input1.nextInt();
+            if( in1 == 2){
+                Scanner inputphone = new Scanner(System.in);
+                String phoneNumber = "";
+                boolean isValid = false;
 
-                        while (!isValid) {
-                            System.out.print("Enter a phone number: ");
-                            phoneNumber = inputphone.nextLine();
-
+                while (!isValid) {
+                    System.out.print("Enter a phone number: ");
+                        phoneNumber = inputphone.nextLine();
                             if (isValidPhoneNumber(phoneNumber)) {
                                 isValid = true;
                             } else {
@@ -53,9 +76,20 @@ public class Project {
                             }
                         }
 
-                        String modifiedPhoneNumber = addZeroToPhoneNumber(phoneNumber);
-                        System.out.println("Phone Number: " + modifiedPhoneNumber);
+                String modifiedPhoneNumber = addZeroToPhoneNumber(phoneNumber);
+                System.out.println("Phone Number: " + modifiedPhoneNumber);
 
+                System.out.println();
+                System.out.println("3.Setting Id");
+                System.out.println("4.Setting Interests");
+
+                Scanner input2 = new Scanner(System.in);
+                int in2 = input2.nextInt();
+                if(in2 == 3){
+                    Userid stdid = new Userid();
+                    String studentid = stdid.Idnum();
+                    System.out.println("Your Student Id : " + studentid);
+                }
             }
         }
 
