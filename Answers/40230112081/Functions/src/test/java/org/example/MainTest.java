a32fc36a4c42b4b81f5f664dbaccb2a5ab28bd9f
Amir <salehamir708@gmail.com>
2024-03-12 16:46:55 +0330

    Test : is created

---
+++ Answers/40230112081/Functions/src/test/java/org/example/MainTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void fullName() {
+    }
+
+    @Test
+    void phoneNumber() {
+    }
+
+    @Test
+    void userID() {
+    }
+
+    @Test
+    void getInterests() {
+    }
+
+    @Test
+    void userFullInformation() {
+    }
+
+    @Test
+    void informationEncoder() {
+    }
+
+    @Test
+    void informationDecoder() {
+    }
+}
