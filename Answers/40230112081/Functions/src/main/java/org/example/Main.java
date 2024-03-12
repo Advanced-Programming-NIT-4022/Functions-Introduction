5983bf7bd6273f83309d9b5f93ece3c25dca34da
Amir <salehamir708@gmail.com>
2024-03-12 11:29:16 +0330

    Functions project is made

---
+++ Answers/40230112081/Functions/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static String normalizeName(String name)
+    {
+        String out = "";
+        char[] chars = name.toCharArray();
+        if((int)chars[0] >= 97 && (int)chars[0] <= 122)
+        {
+            out += String.valueOf((char)((int)chars[0] - 32));
+        }
+        else
+            out += String.valueOf(chars[0]);
+        for(int i = 1;i < chars.length;i++)
+        {
+            if((int)chars[i] <= 90 && (int)chars[i] >= 65)
+            {
+                out += String.valueOf((char)((int)chars[i] + 32));
+            }
+            else
+                out += String.valueOf(chars[i]);
+        }
+        return out;
+    }
+    public static String fullName(String firstName, String lastName){
+        firstName = normalizeName(firstName);
+        lastName = normalizeName(lastName);
+        return firstName + " " + lastName;
+    }
+    
+}
