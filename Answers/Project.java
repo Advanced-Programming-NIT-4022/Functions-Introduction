8e792d0fb9415220d0265f7366dd008ee55276e9
Anndraj <amirrezasolhjo2004@gmail.com>
2024-03-12 17:59:44 +0330

    Added Method : Added string FirstName for capitalizing the first letter

---
+++ Answers/Project.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Project {
+    public static void main(String[] args) {
+        Scanner First = new Scanner(System.in);
+        String first = First.nextLine();
+        Scanner Last = new Scanner(System.in);
+        String last = Last.nextLine();
+        String FirstName = first.substring(0,1).toUpperCase() + first.substring(1).toLowerCase();
+
+    }
+}
