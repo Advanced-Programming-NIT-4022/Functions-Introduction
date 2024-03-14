18ce010193180455468ebc1e78143d55389b7d93
Anndraj <amirrezasolhjo2004@gmail.com>
2024-03-14 15:06:43 +0330

    All Encoder file

--- Answers/Encoder.java
+++ Answers/Encoder.java
@@ -1,22 +1,35 @@
-public static String Caesar(String text, int key) {
-    StringBuilder result = new StringBuilder();
+import java.util.Scanner;
 
-    for (int i = 0; i < text.length(); i++) {
-        char newchar = text.charAt(i);
-
-        if (Character.isLetter(newchar)) {
-            char endchar = (char) ((newchar - 'a' + key) % 26 + 'a');
-            result.append(endchar);
-        } else {
-            result.append(newchar);
-        }
-    }
-
-    return result.toString();
-}
-}
 public class Encoder {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the information to encrypt: ");
+        String userInput = scanner.nextLine();
+
+        System.out.print("Enter the encryption key (an integer): ");
+        int key = scanner.nextInt();
+
+        String endText = Caesar(userInput, key);
+        System.out.println("Encrypted text: " + endText);
+
+        scanner.close();
+    }
+    public static String Caesar(String text, int key) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char newchar = text.charAt(i);
+
+            if (Character.isLetter(newchar)) {
+                char endchar = (char) ((newchar - 'a' + key) % 26 + 'a');
+                result.append(endchar);
+            } else {
+                result.append(newchar);
+            }
+        }
+
+        return result.toString();
     }
 }
+
