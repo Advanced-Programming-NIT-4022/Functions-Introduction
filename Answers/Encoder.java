a1aeda8f4caa2f761f40e45e68d0d51a2cb70aa4
Anndraj <amirrezasolhjo2004@gmail.com>
2024-03-14 14:59:45 +0330

    Added the method for getting char and shift it with user input parameter

--- Answers/Encoder.java
+++ Answers/Encoder.java
@@ -1,2 +1,22 @@
+public static String Caesar(String text, int key) {
+    StringBuilder result = new StringBuilder();
+
+    for (int i = 0; i < text.length(); i++) {
+        char newchar = text.charAt(i);
+
+        if (Character.isLetter(newchar)) {
+            char endchar = (char) ((newchar - 'a' + key) % 26 + 'a');
+            result.append(endchar);
+        } else {
+            result.append(newchar);
+        }
+    }
+
+    return result.toString();
+}
+}
 public class Encoder {
+    public static void main(String[] args) {
+
+    }
 }
