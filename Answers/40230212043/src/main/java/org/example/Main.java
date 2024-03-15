7bf67376a02af75e28fab2af86df55af78f8f69c
aran7227 <161644211+aran7227@users.noreply.github.com>
2024-03-15 19:05:46 +0330

    first is done

---
+++ Answers/40230212043/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+public class Main {
+    static String fullName(String fn,String ln) {
+        String kn=fn+" "+ln;
+        return kn;
+    }
+    public static void main(String[] args) {
+
+    }
+}
