f6c61570ae7fbb50ec7107f3a7fc2e2fc6c5f747
Reza <Rezaaghajanpourpasha@gmail.com>
2024-03-16 23:18:27 +0330

    I added the "getInterests" function and made some positive changes in the others

--- Answers/40230112013/src/Homework.java
+++ Answers/40230112013/src/Homework.java
@@ -13,11 +13,12 @@ public class Homework {
         while (again) {
             System.out.print("Please enter your phone number : ");
             String phonenum = scanner.next();
+            System.out.print("\n");
             phonenum = phoneNumber(phonenum);
             if (phonenum == "F")
                 continue;
             else {
-                System.out.println("your phone number :" + phonenum);
+                System.out.println("your phone number :" + phonenum +"\n");
                 again = false;
                 break;
             }
@@ -32,11 +33,20 @@ public class Homework {
             if (UserIdentifier.startsWith("I"))
                 continue;
             else {
-                System.out.println("Your user ID:" + UserIdentifier);
+                System.out.println("\nYour user ID:" + UserIdentifier + "\n");
                 break;
             }
         }
         // the third function is done
+        String[] interests = getInterests();
+        int i=0;
+        System.out.println("your interest is : ");
+        while (interests[i]!=null) {
+            int y=i+1;
+            String p = String.valueOf(y);
+            System.out.println(p + "_" + interests[i]);
+            i++; 
+        }
         scanner.close();
     }
 
@@ -49,7 +59,7 @@ public class Homework {
         if (lastName != null && lastName.isEmpty() == false) {
             lastName = Character.toUpperCase(lastName.charAt(0)) + lastName.substring(1).toLowerCase();
         }
-        System.out.println("The correct format of your name and surname:" + (firstName) + " " + (lastName));
+        System.out.println("\nThe correct format of your name and surname: " + (firstName) + " " + (lastName)+ "\n");
     }
 
     /// phone number function:
@@ -67,9 +77,42 @@ public class Homework {
     public static String userid(String Uid) {
         if (Uid.length() > 3 && Uid.length() < 15)
             return Uid;
-        else
-            return "Invalid value";
+        else{
+            
+            System.out.println("\nInvalid value , Please enter the correct ID ");
+            return "I";
+        }
 
+    }
+
+    ///getInterests function:
+    public static String[] getInterests() {
+        Scanner scanner = new Scanner(System.in);
+        boolean again = true;
+        String[] array = new String[9];
+        int i = 0;
+        String ask;
+        while (again) {
+            System.out.print("please enter you interest :");
+            array[i] = scanner.next();
+            System.out.print("\n");
+            i++;
+            System.out.println("Do you want to continue? \nyes or no ");
+            ask = scanner.next();
+            System.out.print("\n");
+            switch (ask) {
+                case "yes":  
+                    break;
+                case "no" :
+                again=false;
+                break;
+                default:
+                    {System.out.println("\nThe value is invalid and receiving interests will end\n");
+                    again = false;}
+            }
+        }
+        scanner.close();
+        return array;
     }
 
 }
