5ee76f2ca22c66fdc3288e68f1ef2ce7fad9982c
Sponge Bob <Muhamad22204>
2024-03-16 19:56:04 +0330

    files added

--- Answers/40130212091/Main.java
+++ Answers/40130212091/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        // full name
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your first name :");
         String first_name = sc.next();
@@ -9,6 +10,17 @@ public class Main {
         String last_name = sc.next();
         String full_name = fullName(first_name, last_name);
         System.out.println(full_name);
+        // phone number
+        System.out.println("Enter phone number : ");
+        String phone = sc.next();
+        String newPhone = phoneNumber(phone);
+        while(newPhone == "Wrong"){
+            System.out.println("Wrong entry. Try again.");
+            phone = sc.next();
+            newPhone = phoneNumber(phone);
+        }
+        System.out.println(newPhone);
+        // 
 
     }
 
@@ -16,5 +28,12 @@ public class Main {
         firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
         lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
         return firstName + " " + lastName;
+    }
+
+    public static String phoneNumber(String phone){
+        if (phone.length() == 10 && phone.startsWith("9")){
+            return "0" + phone;
+        }
+        return "Wrong";
     }
 }
