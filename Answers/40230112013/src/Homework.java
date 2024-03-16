763148abcce441823e27d60474030756d575f0c5
Reza <Rezaaghajanpourpasha@gmail.com>
2024-03-16 22:05:52 +0330

    I made some minor changes to the rest of the functions and also wrote the third function

--- Answers/40230112013/src/Homework.java
+++ Answers/40230112013/src/Homework.java
@@ -10,24 +10,38 @@ public class Homework {
         fullName(firstName, LastName);
         // The first function finished and the second function(Phone number) started
         boolean again = true;
-        while (again == true) {
+        while (again) {
             System.out.print("Please enter your phone number : ");
             String phonenum = scanner.next();
             phonenum = phoneNumber(phonenum);
             if (phonenum == "F")
                 continue;
             else {
-                System.out.println(phonenum);
+                System.out.println("your phone number :" + phonenum);
                 again = false;
                 break;
             }
         }
+
         // The second function also done
+        again = true;
+        while (again) {
+            System.out.print("Please enter your user ID : ");
+            String UserIdentifier = scanner.next();
+            UserIdentifier = userid(UserIdentifier);
+            if (UserIdentifier.startsWith("I"))
+                continue;
+            else {
+                System.out.println("Your user ID:" + UserIdentifier);
+                break;
+            }
+        }
+        // the third function is done
         scanner.close();
     }
 
     /// fullname function :
-    
+
     public static void fullName(String firstName, String lastName) {
         if (firstName != null && firstName.isEmpty() == false) {
             firstName = Character.toUpperCase(firstName.charAt(0)) + firstName.substring(1).toLowerCase();
@@ -35,7 +49,7 @@ public class Homework {
         if (lastName != null && lastName.isEmpty() == false) {
             lastName = Character.toUpperCase(lastName.charAt(0)) + lastName.substring(1).toLowerCase();
         }
-        System.out.println((firstName) + " " + (lastName));
+        System.out.println("The correct format of your name and surname:" + (firstName) + " " + (lastName));
     }
 
     /// phone number function:
@@ -47,6 +61,15 @@ public class Homework {
             System.out.println("Invalid phone number. Please try again.");
             return "F";
         }
+    }
+
+    /// userId function:
+    public static String userid(String Uid) {
+        if (Uid.length() > 3 && Uid.length() < 15)
+            return Uid;
+        else
+            return "Invalid value";
+
     }
 
 }
