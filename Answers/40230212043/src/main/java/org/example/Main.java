ad4f1cd6633385bdbb43014c308c4732ce323c61
aran7227 <161644211+aran7227@users.noreply.github.com>
2024-03-17 02:44:55 +0330

    all of them are done

--- Answers/40230212043/src/main/java/org/example/Main.java
+++ Answers/40230212043/src/main/java/org/example/Main.java
@@ -32,7 +32,7 @@ public class Main {
         for (int i=0;i<ra;i++) {
             System.out.println(i+1+". "+algh[i]);
         }
-        System.out.print("You can reach me via my phone number "+t);
+        System.out.println("You can reach me via my phone number "+t+".");
     }
     static String encode(String s,int skh) {
         String result="";
@@ -54,11 +54,89 @@ public class Main {
         }
         return result;
     }
-    static String decode(String s,int mkh) {
-        return encode(s,mkh);
+    static String decode(String u,int mkh) {
+        return encode(u,26-(mkh%26));
     }
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter your first name: ");
+        String fname=sc.nextLine();
+        System.out.print("enter your last name: ");
+        String lname=sc.nextLine();
+        String funame=fullName(fname,lname);
+        System.out.print("enter your phone number: ");
+        String telesho="";
+        for (;;) {
+            String sho=sc.nextLine();
+            telesho=phoneNumber(sho);
+            if (telesho==null) {
+                continue;
+            } else {
+                break;
+            }
+        }
+        System.out.print("enter your student id: ");
+        String checkid="";
+        for (;;) {
+            String Id=sc.nextLine();
+            checkid=userId(Id);
+            if (checkid==null) {
+                continue;
+            } else {
+                break;
+            }
+        }
+        String[] alghe=new String[10];
+        String[] j=new String[10];
+        int p=0;
+        System.out.println("enter your interests. if its enough,enter \"tamam\".");
+        for (;;) {
+            System.out.print("my interest is: ");
+            String adat=sc.nextLine();
+            if (adat.equals("tamam")==true) {
+                break;
+            }
+            j=getInterests(adat,p,alghe);
+            p++;
+        }
+        System.out.println("1.original");
+        System.out.println("2.encode");
+        System.out.println("3.decode");
+        System.out.println("4.exit");
+        System.out.print("choose the number of one option above for showing resume:");
+        String gozine=sc.nextLine();
+        switch(gozine) {
+            case "1":
+                fullinformation(funame, telesho, checkid, j, p);
+                break;
+            case "2":
+                System.out.print("enter a shift number: ");
+                int offset=sc.nextInt();
+                String s1=encode("hello! my name is "+funame+". My ID is "+checkid+". Here are some of my interests:",offset);
+                System.out.println(s1);
+                for (int i=0;i<p;i++) {
+                    int t=i+1;
+                    System.out.println(encode(t+"."+alghe[i],offset));
+                }
+                String s2=encode("You can reach me via my phone number "+telesho+".",offset);
+                System.out.println(s2);
+                break;
+            case "3":
+                System.out.print("enter a shift number: ");
+                int ofset=sc.nextInt();
+                String ss1=decode("hello! my name is "+funame+". My ID is "+checkid+". Here are some of my interests:",ofset);
+                System.out.println(ss1);
+                for (int i=0;i<p;i++) {
+                    int t=i+1;
+                    System.out.println(decode(t+"."+alghe[i],ofset));
+                }
+                String ss2=decode("You can reach me via my phone number "+telesho+".",ofset);
+                System.out.println(ss2);
+                break;
+            case "4":
+                break;
+            default:
+                System.out.println("your option number is wrong!");
         }
     }
 }
