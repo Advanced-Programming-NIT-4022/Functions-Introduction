24e26cfe24a9c2931acb8b4371a631b234849cea
Mostafa <Nasrollahpourmostafa@gmail.com>
2024-03-16 09:20:23 +0330

    add userId method

--- Answers/40230212083/Main.java
+++ Answers/40230212083/Main.java
@@ -9,9 +9,13 @@ public class Main {
         String lname = scan.nextLine();
         System.out.println(fullName(fname , lname));
         */
-
+        /*
         //for calling phoneNumber method
         System.out.println(phoneNumber());
+        */
+
+        //for calling userId method
+        System.out.println(userId());
     }
     public static String fullName(String firstName , String lastName){
         firstName = firstName.toLowerCase();
@@ -21,8 +25,8 @@ public class Main {
         return firstName + " " + lastName ;
     }
     public static String phoneNumber(){
-        Scanner sc = new Scanner(System.in);
-        String number = sc.nextLine();
+        Scanner scan = new Scanner(System.in);
+        String number = scan.nextLine();
         if(number.length() == 10 && number.charAt(0)=='9'){
             return '0' + number;
         }else if(number.length()==11 && number.charAt(0)=='0'){
@@ -30,6 +34,19 @@ public class Main {
         }else{
             System.out.println("Wrong entry. Try again.");
             return phoneNumber();
+        }
+    }
+    public static String userId(){
+        Scanner scan = new Scanner(System.in);
+        String id = scan.nextLine();
+        if(id.length()<4 || id.length()>13){
+            return userId();
+        }else{
+            for(int i=0;i<id.length();i++){
+                if(id.charAt(i)>47 && id.charAt(i)<58) continue; //check for char is number or not
+                else return userId();
+            }
+            return id;
         }
     }
 }
