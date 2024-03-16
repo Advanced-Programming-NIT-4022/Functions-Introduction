0b8efb374af58a659bce8a4f6e1ebee4e08684c4
Yas Hassan Pour <yusshp1989@gmail.com>
2024-03-16 21:18:50 +0330

    userID and some minor correction

--- Answers/Yas Hassanpour_ 40231712003/src/main/java/org/example/Main.java
+++ Answers/Yas Hassanpour_ 40231712003/src/main/java/org/example/Main.java
@@ -27,7 +27,7 @@ public class Main {
         {
             return "0"+phone;
         }
-        if(phone.startsWith("0")&& phone.length()==10)
+        if(phone.startsWith("0")&& phone.length()>=10)
         {
             return phone;
         }
@@ -37,6 +37,18 @@ public class Main {
         }
         return "Unknown error! Please check the input.";
     }
+    static String userID(String id)
+    {
+        if(id.length()>=4 && id.length()<=13)
+        {
+            return id;
+        }
+        else
+        {
+            return "Invalid user ID. Try again.";
+        }
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Please enter your name: ");
@@ -45,16 +57,27 @@ public class Main {
         System.out.println("Please enter your last name: ");
         Scanner inputL= new Scanner(System.in);
         String lastName= inputL.nextLine();
-        //System.out.println(fullName(firstName,lastName));
-        String returnedValue = phoneNumber("456743");
+        System.out.println(fullName(firstName,lastName));
+        String returnedValue;
         do {
-
             System.out.println("Please enter your phone number: ");
             Scanner phNumberInput = new Scanner(System.in);
             String phNumber = phNumberInput.nextLine();
-            System.out.println(phoneNumber(phNumber));
+            returnedValue = phoneNumber(phNumber);
+            System.out.println(phoneNumber(returnedValue));
         }
         while (returnedValue.equals("Wrong entry! Try again."));
+        String returnedValueID;
+
+        do {
+            System.out.println("Please enter your user ID: ");
+            Scanner idInput = new Scanner(System.in);
+            String idNum = idInput.nextLine();
+            returnedValueID = userID(idNum);
+            System.out.println(returnedValueID);
+        }
+        while (returnedValueID.equals("Invalid user ID. Try again."));
+
 
     }
 
