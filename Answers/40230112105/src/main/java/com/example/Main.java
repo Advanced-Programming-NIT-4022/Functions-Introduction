247f54ba90929a8438df8b04d381ffe0ee10baf1
Nima <nimafeli138384@gmail.com>
2024-03-16 19:53:35 +0330

    complete getInterests function and add main function to call other functions

--- Answers/40230112105/src/main/java/com/example/Main.java
+++ Answers/40230112105/src/main/java/com/example/Main.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    static String fullName(String fistName,String lastName) {
+    static String fullName(String fristName,String lastName) {
         fristName = fristName.toLowerCase();
         lastName = lastName.toLowerCase();
         fristName = fristName.substring(0,1).toUpperCase() + fristName.substring(1);
         lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
-        return firstName + " " + lastName;
+        return fristName + " " + lastName;
 
     }
 
     static String phoneNumber(long number) {
-        Scanner intScanner = new Scanner(System.in);
-        String strNumber;
+        Scanner longScanner = new Scanner(System.in);
+        String strNumber = "999";
         int size;
         boolean flag = false;
         while(!flag){
             strNumber = String.valueOf(number);
             size = strNumber.length();
-            if (strNumber.substring(0, 1).equals("9") && size == 9) {
-                falg = true;
+            if (strNumber.substring(0, 1).equals("9") && size == 10) {
+                flag = true;
             } else {
                 System.out.println("Wrong entry. Try again");
-                number = intScanner.nextInt();
+                number = longScanner.nextLong();
             }
         }
         return "0" + strNumber;
@@ -34,7 +34,7 @@ public class Main {
         boolean flag = false;
         while(!flag) {
             strId = String.valueOf(id);
-            if(strId.lenght() <= 13 && strId.length() >= 4){
+            if(strId.length() <= 13 && strId.length() >= 4){
                 flag = true;
             } else {
                 System.out.println("Wrong entry. Try again");
@@ -43,4 +43,58 @@ public class Main {
         }
         return id;
     }
+
+    static String[] getInterests(String[] interests) { 
+        Scanner strScanner = new Scanner(System.in);
+        String interest = "NIMA";
+        int index = 0;
+        while (index != 10) {
+            System.out.print("give me your interest(if you dont have more interests type\"end\"): ");
+            interest = strScanner.next();
+            if (interest.equals("end")) {
+                break;
+            } else {
+                interests[index] = interest;
+                index++;
+            }
+        }
+        
+        return interests;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("RUNNIG >>> fullName function");
+        Scanner strScanner = new Scanner(System.in);
+        System.out.print("please enter your first name: ");
+        String name = strScanner.next();
+        System.out.print("please enter your last name: ");
+        String last = strScanner.next();
+        System.out.println(fullName(name, last));
+        ////////////////////////////////////////////////
+        System.out.println("RUNNING >>> phoneNumber function");
+        Scanner longScanner = new Scanner(System.in);
+        System.out.print("please enter your number witout zero ate the first: ");
+        long number = longScanner.nextLong();
+        System.out.println(phoneNumber(number));
+        ////////////////////////////////////////////////
+        System.out.println("RUNNING >>> userId function");
+        System.out.print("please give me a standard ID: ");
+        long ID = longScanner.nextLong();
+        System.out.println(userId(ID));
+        ////////////////////////////////////////////////
+        System.out.println("RUNNING >>> getInterests function");
+        String[] interests = new String[10];
+        interests = getInterests(interests);
+        int i = 0;
+        System.out.print("interests: {");
+        while (interests[i] != null) {
+            System.out.print("\"" + interests[i] + "\" ");
+            i++;
+        }
+        System.out.println("}");
+        /////////////////////////////////////////////////
+        
+    }
+
 }
