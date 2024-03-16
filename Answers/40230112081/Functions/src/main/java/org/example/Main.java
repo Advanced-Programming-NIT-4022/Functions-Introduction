400f848db3d1d64454c927722486382de0aacd67
Amir <salehamir708@gmail.com>
2024-03-16 19:58:57 +0330

    Main file is completed and Include some tests for functions

--- Answers/40230112081/Functions/src/main/java/org/example/Main.java
+++ Answers/40230112081/Functions/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
 public class Main {
-    Functions obj = new Functions();
+    
     public static void main(String[] args) {
-        
+        Functions obj = new Functions();
+        System.out.println(obj.fullName("NIma","fARkhOONDEh"));
+        Vector v = new Vector();
+        System.out.println(obj.getInterests(v).length);
+        String[] arr = v.get_arr();
+        obj.userFullInformation("Nima", "09121234567", "40230112081", arr);  
     }
 }
