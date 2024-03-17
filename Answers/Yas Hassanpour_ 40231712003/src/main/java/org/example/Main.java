e038bba545ebb212fa9816a75b0e3c12273c9068
Yas Hassan Pour <yusshp1989@gmail.com>
2024-03-17 13:47:25 +0330

    minor improvements

--- Answers/Yas Hassanpour_ 40231712003/src/main/java/org/example/Main.java
+++ Answers/Yas Hassanpour_ 40231712003/src/main/java/org/example/Main.java
@@ -27,11 +27,11 @@ public class Main {
         {
             return "0"+phone;
         }
-        if(phone.startsWith("0")&& phone.length()>=10)
+        if(phone.startsWith("0")&& phone.length()==11)
         {
             return phone;
         }
-        if(!phone.startsWith("9")||phone.length()<10)
+        if(!phone.startsWith("9")||phone.length()<10||phone.length()>11)
         {
             return "Wrong entry! Try again.";
         }
@@ -51,18 +51,16 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Please enter your name: ");
+        System.out.print("Please enter your name: ");
         Scanner input= new Scanner(System.in);
         String firstName= input.nextLine();
-        System.out.println("Please enter your last name: ");
-        Scanner inputL= new Scanner(System.in);
-        String lastName= inputL.nextLine();
+        System.out.print("Please enter your last name: ");
+        String lastName= input.nextLine();
         System.out.println(fullName(firstName,lastName));
         String returnedValue;
         do {
-            System.out.println("Please enter your phone number: ");
-            Scanner phNumberInput = new Scanner(System.in);
-            String phNumber = phNumberInput.nextLine();
+            System.out.print("\nPlease enter your phone number: ");
+            String phNumber = input.nextLine();
             returnedValue = phoneNumber(phNumber);
             System.out.println(phoneNumber(returnedValue));
         }
@@ -70,9 +68,8 @@ public class Main {
         String returnedValueID;
 
         do {
-            System.out.println("Please enter your user ID: ");
-            Scanner idInput = new Scanner(System.in);
-            String idNum = idInput.nextLine();
+            System.out.print("\nPlease enter your user ID: ");
+            String idNum = input.nextLine();
             returnedValueID = userID(idNum);
             System.out.println(returnedValueID);
         }
