fe72c56b54be74cc70623fb76386cbb5a14e5b21
The Navid <thenavid2005@gmail.com>
2024-03-19 20:01:49 +0330

    phoneNumber

--- Answers/40230212013/functionsIntroduction.java
+++ Answers/40230212013/functionsIntroduction.java
@@ -9,6 +9,16 @@ public class functionsIntroduction {
   {
     return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
   }
+  /********************************************************************************************************************************************************************************** */
+  public static String phoneNumber(String phone) {
+    if (phone.length() == 10 && phone.startsWith("9")) {
+        return "0" + phone;
+    } else {
+        System.out.println("Wrong entry. Try again.");
+        return phoneNumber(new Scanner(System.in).nextLine());
+    }
+  }
+
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter your First Name: ");
@@ -16,5 +26,8 @@ public class functionsIntroduction {
     System.out.println("Enter your Last Name: ");
     String lastName = input.nextLine();
     String fullName = fullName(firstName, lastName);
+    System.out.println("Enter your Phone Number: ");
+    String phone = phoneNumber(input.nextLine());
+
   }
 }
