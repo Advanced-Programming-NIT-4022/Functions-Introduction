b505f3169fb42152d3f400173d6dfe52de282915
mohammadhm180 <mohammadhm180@gmail.com>
2024-03-20 22:10:06 +0330

    add phoneNumber function

--- Answers/40131712053/src/Main.java
+++ Answers/40131712053/src/Main.java
@@ -7,5 +7,27 @@ public class Main {
         return firstName + " " + lastName;
     }
 
+    public static String phoneNumber(String phone){
+        boolean isAllDigits = true;
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                isAllDigits = false;
+                break;
+            }
+        }
+        if (!isAllDigits) {
+            return "wrong";
+        }
+        if(phone.length() !=10){
+            return "wrong";
+        }
+        if(phone.charAt(0)!='9'){
+            return "wrong";
+        }
+        return "0"+phone;
+    }
+
+
+
 
 }
