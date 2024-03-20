b38245a034e6ef270e75862b8d4aabeac825d69c
mohammadhm180 <mohammadhm180@gmail.com>
2024-03-20 21:20:26 +0330

    add fullName function

---
+++ Answers/40131712053/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
+
+
+}
