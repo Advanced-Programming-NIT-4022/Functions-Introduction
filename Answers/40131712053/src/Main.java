0383b2e65aef50771aaece961534890b3767cc91
mohammadhm180 <mohammadhm180@gmail.com>
2024-03-20 23:26:58 +0330

    fix functions to get input

--- Answers/40131712053/src/Main.java
+++ Answers/40131712053/src/Main.java
@@ -7,43 +7,67 @@ public class Main {
 
     }
 
-    public static String fullName(String firstName, String lastName){
+    public static String fullName(){
+        String firstName="";
+        String lastName="";
+        System.out.println("first name:");
+        while (firstName.equals("")){
+            firstName=scanner.nextLine();
+        }
+        System.out.println("last name:");
+        while (lastName.equals("")){
+            lastName=scanner.nextLine();
+        }
+
         return firstName + " " + lastName;
     }
 
-    public static String phoneNumber(String phone){
-        boolean isAllDigits = true;
-        for (int i = 0; i < phone.length(); i++) {
-            if (!Character.isDigit(phone.charAt(i))) {
-                isAllDigits = false;
-                break;
+    public static String phoneNumber(){
+        System.out.println("Phone Number:");
+        String phone="wrong";
+        while (phone.equals("wrong")){
+            phone=scanner.nextLine();
+            boolean isAllDigits = true;
+            for (int i = 0; i < phone.length(); i++) {
+                if (!Character.isDigit(phone.charAt(i))) {
+                    isAllDigits = false;
+                    break;
+                }
             }
-        }
-        if (!isAllDigits) {
-            return "wrong";
-        }
-        if(phone.length() !=10){
-            return "wrong";
-        }
-        if(phone.charAt(0)!='9'){
-            return "wrong";
+            if (!isAllDigits) {
+                System.out.println("please enter only digits");
+                phone= "wrong";
+            } else if(phone.length() !=10){
+                System.out.println("please enter 10 digits");
+                phone= "wrong";
+            } else if(phone.charAt(0)!='9'){
+                System.out.println("the first digit must be 9");
+                phone= "wrong";
+            }
         }
         return "0"+phone;
     }
 
-    public static String userId(String id){
-        boolean isAllDigits = true;
-        for (int i = 0; i < id.length(); i++) {
-            if (!Character.isDigit(id.charAt(i))) {
-                isAllDigits = false;
-                break;
+    public static String userId(){
+        String id="wrong";
+        System.out.println("user ID:");
+        while (id.equals("wrong")){
+            id=scanner.nextLine();
+            boolean isAllDigits = true;
+            for (int i = 0; i < id.length(); i++) {
+                if (!Character.isDigit(id.charAt(i))) {
+                    isAllDigits = false;
+                    break;
+                }
             }
-        }
-        if (!isAllDigits) {
-            return "wrong";
-        }
-        if(id.length()<4 || id.length()>13){
-            return "wrong";
+            if (!isAllDigits) {
+                System.out.println("please enter only digits");
+                id= "wrong";
+            }
+            if(id.length()<4 || id.length()>13){
+                System.out.println("id length must be between 4 and 13");
+                id= "wrong";
+            }
         }
         return id;
     }
