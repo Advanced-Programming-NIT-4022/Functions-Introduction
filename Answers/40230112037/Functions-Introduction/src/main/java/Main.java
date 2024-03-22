afce9c3aa6e56803ff13294d36290385742471a9
HJavid010 <hosein.javid.010@gmail.com>
2024-03-22 23:12:55 +0330

    Added normalizeName function

--- Answers/40230112037/Functions-Introduction/src/main/java/Main.java
+++ Answers/40230112037/Functions-Introduction/src/main/java/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static String normalizeName(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
 }
