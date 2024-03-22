fec40b21aec5a20e589b018636c3c383496ebd4f
HJavid010 <hosein.javid.010@gmail.com>
2024-03-22 23:14:07 +0330

    Added phoneNumber function

--- Answers/40230112037/Functions-Introduction/src/main/java/Main.java
+++ Answers/40230112037/Functions-Introduction/src/main/java/Main.java
@@ -20,4 +20,28 @@ public class Main {
         lastName = normalizeName(lastName);
         return firstName + " " + lastName;
     }
+
+    public static String phoneNumber(String phone) {
+        switch (phone.length()) {
+            case 10:
+                if (phone.charAt(0) != '9') {
+                    throw new Error("invalid phone number! (enter phone number like 09xxxxxxxxx or 9xxxxxxxxx)");
+                }
+                phone = "0" + phone;
+                break;
+            case 11:
+                if (phone.charAt(0) != '0' || phone.charAt(1) != '9') {
+                    throw new Error("invalid phone number! (enter phone number like 09xxxxxxxxx or 9xxxxxxxxx)");
+                }
+                break;
+            default:
+                throw new Error("invalid phone number! (phone number length should be 10 or 11)");
+        }
+        for (char i : phone.toCharArray()) {
+            if (i < '0' || i > '9') {
+                throw new Error("invalid phone number! (phone number should only contains numbers)");
+            }
+        }
+        return phone;
+    }
 }
