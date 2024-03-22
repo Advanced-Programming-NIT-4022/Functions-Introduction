f54afdb0d43d1a1a2f131fc996d295be9a2a0061
Mohammad Hossein Heidary <mmdmshti@gmail.com>
2024-03-22 13:18:09 +0330

    fixed bugs!

--- Answers/40230112050/project.java
+++ Answers/40230112050/project.java
@@ -9,18 +9,47 @@ public class project {
         First_name = input.nextLine();
         System.out.println("please enter your last_name: ");
         Last_name = input.nextLine();
+        String alfabet = "abcdefghijklmnopqrstuvwxyz";
+        String[]alfabet1 = {"a" , "b" , "c" , "d", "e","f","g","h" , "i" , "j" , "k" ,"l" ,"m" , "n", "o" , "p" ,"q","r","s","t","u","v", "w" , "x", "y","z"};
+
+        int i = 3;
+
+
+
+        String testo ="Hello! My name is " + Fullname(First_name , Last_name) +". My ID is "+ ID(ID) +".here are some of my intrests:"+ intrest(intrest) + "\nyou can reach me via my phone number "+ final_phonenumber(phonenumber);
+        //System.out.format("Hello! My name is %s. My ID is %s.here are some of my intrests:%s \nyou can reach me via my phone number %s " , Fullname(First_name , Last_name) , ID(ID) , intrest(intrest) , final_phonenumber(phonenumber));
+        String test = testo.toLowerCase();
+        String[]test1 = test.split("");
+        System.out.println("\ndo you want to see the encoded result: ");
+        String y = input.nextLine();
+        if(Objects.equals(y, "no") || Objects.equals(y, "-"))
+            System.out.println(testo);
+        else if(Objects.equals(y, "yes") || Objects.equals(y, "+")  ) {
+            for (int x = 0; x < test1.length; x++) {
+                if (test1[x] == " ") continue;
+                int f = alfabet.indexOf(test1[x]) + i;
+                if (f > 26)
+                    f %= 26;
+                test1[x] = alfabet1[f];
+                System.out.print(test1[x]);
+            }
+        }
 
 
 
 
 
-        System.out.format("Hello! My name is %s. My ID is %s.here are some of my intrests:%s \nyou can reach me via my phone number %s " , Fullname(First_name , Last_name) , ID(ID) , intrest(intrest) , final_phonenumber(phonenumber));
+
+
+
+
 
     }
 
     public static String Fullname(String First_name, String last_name) {
         First_name = First_name.toLowerCase();
         last_name = last_name.toLowerCase();
+
 
         String[] list = First_name.split("");
         String[] list_2 = last_name.split("");
@@ -99,13 +128,14 @@ public class project {
         for(int i = 0 ; i < 10 ; i++){
             Scanner input = new Scanner(System.in);
             intrest = input.nextLine();
-            if(intrest == "end")break;
 
             intrests_list[i] = intrest;
+            if(intrests_list[i] == "end")break;
         }
 
 
         for(int x = 1 ; x < 11 ; x++){
+
             list[0] += "\n" + x + "_" + intrests_list[x - 1];
         }
         return list[0];
