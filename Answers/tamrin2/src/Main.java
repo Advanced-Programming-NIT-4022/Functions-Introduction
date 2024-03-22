d8c7dda5d2a50131176a84c79235ad5b836cee5b
Amir Hosyn Fuladi Rostami <darksilentcore@gmail.com>
2024-03-22 16:58:43 +0330

    fn 2 done

--- Answers/tamrin2/src/Main.java
+++ Answers/tamrin2/src/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
-        System.out.println("Full Name: " + fullName("ArYAn", "nourBAKhsh") + "\n");
-    }
-    static String fullName(String firstName, String lastName) {
-        return firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()
-                + " " + lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase();
+	static String fullName(String firstName, String lastName) {
+		return firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()
+				+ " " + lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase();
+	}
+	static String phoneNumber(String phone) {
+		if ((phone.charAt(0) == '9') && (phone.length() == 10)) {
+			return "0" + phone;
+		} else {
+			return "error";
+		}
+	}
+
+	public static void main(String[] args) {
+//        System.out.println("Full Name: " + fullName("ArYAn", "nourBAKhsh") + "\n");
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String phone_test = input.nextLine();
+			if (phoneNumber(phone_test).equals("error")) {
+				;
+			} else {
+				System.out.println(phoneNumber(phone_test));
+				break;
+			}
+        }
+		input.close();
     }
 }
