bee2eaa3df51a8ab943e41953930c4adae9a2e06
Amir Hosyn Fuladi Rostami <darksilentcore@gmail.com>
2024-03-22 18:32:31 +0330

    fn 5 done (userFullInformation)

--- Answers/tamrin2/src/Main.java
+++ Answers/tamrin2/src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 class Main {
 	static String fullName(String firstName, String lastName) {
-		return firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()
-				+ " " + lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase();
+		return firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase()
+				+ " " + lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
 	}
+
 	static String phoneNumber(String phone) {
 		if ((phone.charAt(0) == '9') && (phone.length() == 10)) {
 			return "0" + phone;
@@ -12,6 +13,7 @@ class Main {
 			return "error";
 		}
 	}
+
 	static String userId(String id) {
 		if ((4 <= id.length()) && (id.length() <= 13)) {
 			return id;
@@ -19,13 +21,14 @@ class Main {
 			return "error";
 		}
 	}
+
 	static String[] getInterests() {
 		System.out.println("0 = quit");
 		String[] array = new String[10];
 		Scanner scanner_input = new Scanner(System.in);
-		for (int i = 0; i < 10;) {
+		for (int i = 0; i < 10; ) {
 			System.out.print("Type: ");
-			String input_test =  scanner_input.nextLine();
+			String input_test = scanner_input.nextLine();
 			if (input_test.equals("0")) {
 				break;
 			} else {
@@ -35,6 +38,20 @@ class Main {
 		}
 		return array;
 	}
+
+	static void userFullInformation(String fullName, String phoneNumber, String userID, String[] interests) {
+		System.out.println("Hi xD");
+		System.out.println("My name is " + fullName + ".");
+		System.out.println("My ID is " + userID + ".");
+		System.out.println("Here are some of my interests:");
+		for (int i = 0; i < interests.length; i++) {
+			System.out.println((i + 1) + ". " + interests[i]);
+		}
+		System.out.println();
+		System.out.println("You can reach me via my phone number " + phoneNumber + ".");
+		System.out.println("GoodBye xD");
+	}
+
 
 	public static void main(String[] args) {
 //        System.out.println("Full Name: " + fullName("ArYAn", "nourBAKhsh") + "\n");
@@ -65,11 +82,14 @@ class Main {
 //		}
 //		input.close();
 
-		String[] amir_interests_array = new String[10];
-		amir_interests_array = getInterests();
-		for (int i = 0; i < 10; i++) {
-			System.out.println("arr[" + i + "] = " + amir_interests_array[i]);
-		}
+//		String[] amir_interests_array = new String[10];
+//		amir_interests_array = getInterests();
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("arr[" + i + "] = " + amir_interests_array[i]);
+//		}
 
-    }
+		String[] sample_interest_array = {"swimming", "gym", "video games"};
+		userFullInformation("Aryan Nourbakhsh", "09123456789", "40030111111", sample_interest_array);
+
+	}
 }
