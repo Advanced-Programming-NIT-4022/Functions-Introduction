372f20912549b415c896be0e351cdce1a8fffb52
seyedkosarmousavi <seyedkosarmousavi2005@gmail.com>
2024-03-22 13:17:02 +0330

    wrigt fullname function

--- Answers/40231712002/Main.java
+++ Answers/40231712002/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static String fullname(String firstname, String lastname) {
+        String firstCharFirstname = firstname.substring(0, 1).toUpperCase();
+        String firstCharLastname = lastname.substring(0, 1).toUpperCase();
+        String otherfirstname = firstname.substring(1).toLowerCase();
+        String otherlastname = lastname.substring(1).toLowerCase();
+
+        return ("Name:" + firstCharFirstname + otherfirstname + firstCharLastname + otherlastname);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fullname("kosar ", "mousavi"));
+    }
 }
