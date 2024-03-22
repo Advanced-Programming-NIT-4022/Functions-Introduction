0e7a90f65bef2c58f6459f50f916a60b18f7259f
HJavid010 <hosein.javid.010@gmail.com>
2024-03-22 23:13:26 +0330

    Added fullName function

--- Answers/40230112037/Functions-Introduction/src/main/java/Main.java
+++ Answers/40230112037/Functions-Introduction/src/main/java/Main.java
@@ -8,4 +8,16 @@ public class Main {
     public static String normalizeName(String name) {
         return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
     }
+
+    public static String fullName(String firstName, String lastName) {
+        if (firstName.isBlank()) {
+            throw new Error("firstName is Empty!");
+        }
+        if (lastName.isBlank()) {
+            throw new Error("lastName is Empty!");
+        }
+        firstName = normalizeName(firstName);
+        lastName = normalizeName(lastName);
+        return firstName + " " + lastName;
+    }
 }
